0f707025bf3aa2d7b515086fd74c275f6765e1f5
Asamzilikazi <asamzilikazi@gmail.com>
2016-03-27 22:32:04 +0200

    Dependency inversion principle

---
+++ Supervisor.java
@@ -0,0 +1,11 @@
+package za.ac.mzilikazi.fixed;
+
+/**
+ * Created by mandisi on 2016-03-27.
+ */
+public class Supervisor implements EmployeeInterface{
+    public void work() {
+
+        System.out.println("Working hard");
+         }
+}
