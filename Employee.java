f58280cb908869dd47fd5ae5d1df8481caab157f
Asamzilikazi <asamzilikazi@gmail.com>
2016-03-27 22:29:06 +0200

    Dependency inversion principle

---
+++ Employee.java
@@ -0,0 +1,12 @@
+package za.ac.mzilikazi.fixed;
+
+/**
+ * Created by mandisi on 2016-03-27.
+ */
+public class Employee implements EmployeeInterface{
+
+    public void work() {
+
+        System.out.println("average work");
+        }
+}
